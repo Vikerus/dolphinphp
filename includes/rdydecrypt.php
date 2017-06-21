d14885e65ad3279ea209ccc43ee92798f34aa833
Bikerus the 1st <Vikerus@users.noreply.github.com>
2017-06-21 02:09:46 -0700

    Update rdydecrypt.php

--- includes/rdydecrypt.php
+++ includes/rdydecrypt.php
@@ -12,15 +12,12 @@ $senttohash = $_POST["hashid"];
 $encryptionMethod = "AES-256-CBC";  // AES is used by the U.S. gov't to encrypt top secret documents.
 $secretHash = "$senttohash";
 
-$iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC);
-$iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
 
 $encryptedMessage = $_POST["messagekey"];
 $decryptedMessage = openssl_decrypt($encryptedMessage, $encryptionMethod, $secretHash, 0, $iv);
 
 $data = $iv.$encryptedMessage;
 
-$iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC);
 $iv = substr($data, 0, $iv_size);
 $decryptedMessage = openssl_decrypt(substr($data, $iv_size), $encryptionMethod, $secretHash, 0, $iv);
 //Result
